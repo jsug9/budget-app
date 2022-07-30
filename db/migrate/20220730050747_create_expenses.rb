1460570f10256fad03af397af23e12569171017a
Augusto <jesus_augga_99@icloud.com>
2022-07-30 00:09:32 -0500

    add expenses mvc

---
+++ db/migrate/20220730050747_create_expenses.rb
@@ -0,0 +1,11 @@
+class CreateExpenses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :expenses do |t|
+      t.references :author, foreign_key: { to_table: :users }, null: false
+      t.string :name
+      t.float :amount
+
+      t.timestamps
+    end
+  end
+end
