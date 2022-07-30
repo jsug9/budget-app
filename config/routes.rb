3a0d6a6b9d420b8fb5d0101a324d84a824440211
Augusto <jesus_augga_99@icloud.com>
2022-07-30 16:12:58 -0500

    remove update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :group_expenses
-  resources :expenses, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  resources :groups, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  resources :users, only: [:index, :show]
+  resources :expenses
+  resources :groups
+  resources :users
 
   root 'groups#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
