e2e3b16bf5af2826f90777f1ab0d42bb19088d99
Augusto <jesus_augga_99@icloud.com>
2022-07-30 10:12:28 -0500

    add user reference to groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_30_050747) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_30_140019) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,11 +23,22 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_30_050747) do
     t.index ["author_id"], name: "index_expenses_on_author_id"
   end
 
+  create_table "group_expenses", force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "expense_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["expense_id"], name: "index_group_expenses_on_expense_id"
+    t.index ["group_id"], name: "index_group_expenses_on_group_id"
+  end
+
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.string "icon"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_30_050747) do
   end
 
   add_foreign_key "expenses", "users", column: "author_id"
+  add_foreign_key "group_expenses", "expenses"
+  add_foreign_key "group_expenses", "groups"
+  add_foreign_key "groups", "users"
 end
