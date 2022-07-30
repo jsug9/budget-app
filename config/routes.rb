124d80262d2b21c8f0b35429b75dca1aeb2a5bda
Augusto <jesus_augga_99@icloud.com>
2022-07-30 12:20:51 -0500

    add root to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :group_expenses
-  resources :expenses
-  resources :groups
-  resources :users
+  resources :expenses, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :groups, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :users, only: [:index, :show]
+
+  root 'groups#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
