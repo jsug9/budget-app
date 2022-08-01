f0d2d5e3cca45ee953c45965acaf456eecacf489
Augusto <jesus_augga_99@icloud.com>
2022-08-01 17:31:59 -0500

    add total amount method to group

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,12 @@ class Group < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :group_expense, dependent: :destroy
   has_many :expenses, through: :group_expense
+
+  def total_amount
+    total = 0
+    expenses.each do |expense|
+      total += expense.amount
+    end
+    total
+  end
 end
