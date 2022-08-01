cebc41bc168722719d930d1f3b0ee26f3497464d
Augusto <jesus_augga_99@icloud.com>
2022-08-01 12:05:45 -0500

    update has_many relationships

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :group_expense
+  has_many :group_expense, dependent: :destroy
+  has_many :expenses, through: :group_expense
 end
