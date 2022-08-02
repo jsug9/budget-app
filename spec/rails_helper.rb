0ac45adcae9bef87e22358720928f17cecda4e16
Augusto <jesus_augga_99@icloud.com>
2022-08-02 09:51:18 -0500

    add tests for group controller

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,6 +38,8 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Devise::Test::IntegrationHelpers, type: :feature
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
