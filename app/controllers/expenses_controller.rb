0e1125e7ca984ab29a311128d34194cae1dd8c8b
Augusto <jesus_augga_99@icloud.com>
2022-08-01 23:29:21 -0500

    Add delete button for expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -52,7 +52,7 @@ class ExpensesController < ApplicationController
     @expense.destroy
 
     respond_to do |format|
-      format.html { redirect_to expenses_url, notice: 'Expense was successfully destroyed.' }
+      format.html { redirect_to @group, notice: 'Expense was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
