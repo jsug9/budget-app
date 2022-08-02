1b67598e6818aab088589f83199bf0b7912cbba4
Augusto <jesus_augga_99@icloud.com>
2022-08-01 19:40:22 -0500

    add validations

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,6 +3,9 @@ class Group < ApplicationRecord
   has_many :group_expense, dependent: :destroy
   has_many :expenses, through: :group_expense
 
+  validates :name, presence: true
+  validates :icon, presence: true
+
   def total_amount
     expenses.sum(:amount)
   end
